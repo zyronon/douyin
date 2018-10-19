ee491431fa306f19c715cef9add9cbbec5ce1455
zyronon <zyronon@163.com>
2018-10-19 17:09:34 +0800

    消息页面完成80%

---
+++ src/components/me/Me.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="me">
+        <Footer v-bind:init-tab="5"/>
+    </div>
+</template>
+
+<script>
+    import Footer from '../common/Footer';
+
+    export default {
+        name: "Me",
+        components: {
+            Footer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
