49094346ad00bec30b970243b51f096138296852
ttentau <ttentau@163.com>
2019-05-20 00:18:58 +0800

    添加音乐页面

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,13 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '' : '',
     assetsDir: './',//资源路径
     outputDir: process.env.outputDir,//打包输出路径
+    configureWebpack: config => {
+        config.externals = {
+            "vue": "Vue",
+            "vue-router": "VueRouter",
+        }
+    },
+    // 是否为生产环境构建生成 source map
+    productionSourceMap: false,
+    css: {},
 }
