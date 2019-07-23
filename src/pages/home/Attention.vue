1b3b565148f05b34593507dd00a84123826b8100
zyronon <zyronon@163.com>
2019-07-24 00:24:57 +0800

    bf

---
+++ src/pages/home/Attention.vue
@@ -0,0 +1,218 @@
+<template>
+    <div id="attention" >
+        <div class="items p10p">
+            <div class="item mb20p pb20p" v-for="(item,index) of items" :key="index" >
+                <div class="header mb10p">
+                    <img src="../../assets/img/icon/head-image.jpeg" alt="">
+                    <span class="ml5p">TTentau</span>
+                </div>
+                <div class="my-comment mb10p">
+                    最靠近北极有个不生不来之地，北极熊比人还多，对中国免签 <span class="tag">#带他去旅行</span>
+                </div>
+                <div class="other-comment p10p">
+                    <span class="user-name">@super-man:</span>
+                    <span class="tag">#购物车里是什么</span>
+                    <span>无敌浩克</span>
+                </div>
+                <div class="video-container mb10p ">
+                    <video src="../../assets/video/吴三二的光年之外.mp4" poster="../../assets/img/poster/src1-bg.png"></video>
+                    <div class="music">
+                        <img src="../../assets/img/icon/music.svg" alt="" class="music-image">
+                        <marquee behavior=scroll direction=left align=middle scrollamount=4> 吴三二 - 光年之外</marquee>
+                        <img src="../../assets/img/icon/play.svg" alt="" class="play">
+                        <img src="../../assets/img/icon/pause.svg" alt="" class="pause" v-if="false">
+                    </div>
+                </div>
+                <div class="location mb10p">
+                    <img src="../../assets/img/icon/location.svg" alt="">
+                    <span class="ml5p">东方明珠</span>
+                </div>
+                <div class="toolbar mb10p">
+                    <div class="item">
+                        <img src="../../assets/img/icon/love.svg" alt="">
+                        <img src="../../assets/img/icon/loved.svg" alt="" class="love-image" v-if="false">
+                        <span class="count">555</span>
+                    </div>
+                    <div class="item">
+                        <img src="../../assets/img/icon/message.svg" alt="">
+                        <span class="count">555</span>
+                    </div>
+                    <div class="item">
+                        <img src="../../assets/img/icon/love.svg" alt="">
+                        <span class="count">555</span>
+                    </div>
+                    <div class="item">
+                        <img src="../../assets/img/icon/love.svg" alt="">
+                        <span class="count">555</span>
+                    </div>
+                </div>
+                <div class="hot-comment pr15p mb5p  oh">
+                    <div class="content pull-left ">
+                        <img src="../../assets/img/icon/head-image.jpeg" alt="" class="head-image pull-left">
+                        <div class="pull-right comment-container">
+                            <div class="name">@ttentau</div>
+                            <div class="detail">其实我也不知道评论点什么啊啊啊看吧看吧看吧</div>
+                        </div>
+                    </div>
+                    <div class="love pull-right">
+                        <img src="../../assets/img/icon/love-gray.svg" alt="" class="love-image">
+                        <img src="../../assets/img/icon/loved.svg" alt="" class="love-image" v-if="false">
+                        <span class="f14 block">3453</span>
+                    </div>
+                </div>
+                <span class="time">11小时前</span>
+            </div>
+        </div>
+        <Footer v-bind:init-tab="2"  style="position: fixed;" />
+    </div>
+</template>
+
+<script>
+
+    import Footer from '../../components/Footer';
+
+    export default {
+        name: "Attention",
+        components: {
+            Footer
+        },
+        data(){
+            return {
+                items:[
+                    {},{},{}
+                ],
+                height:0,
+                width:0,
+                footerHeight:0
+            }
+        },
+        created() {
+            this.height = document.body.clientHeight;
+            this.width = document.body.clientWidth;
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    #attention {
+        /*background: rgb(22，24，34);*/
+        /*background: #161822;*/
+        //抖音原色，感觉很暗
+        background: #2e3244;
+        color: #b8b9c1;
+        position: relative;
+        .items {
+            >.item {
+                border-bottom: 1px solid #494950;
+                .header {
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                    img {
+                        width: 40px;
+                        height: 40px;
+                        border-radius: 50%;
+                    }
+                }
+                .other-comment {
+                    display: none;
+                    background: #15161b;
+                    border-radius: 10px 10px 0 0;
+                }
+                .video-container {
+                    position: relative;
+                    width: 80%;
+                    video {
+                        border-radius: 10px 10px  10px 10px;
+                        width: 100%;
+                    }
+                    .music {
+                        position: absolute;
+                        bottom: 15px;
+                        right: 15px;
+                        left: 15px;
+                        .music-image {
+                            width: 20px;
+                            height: 20px;
+                            margin-top: 3px;
+                            position: absolute;
+                        }
+                        marquee {
+                            width: 50%;
+                            margin-left: 30px;
+                            color: white;
+                        }
+                        .play, .pause {
+                            width: 20px;
+                            height: 20px;
+                            margin-top: 3px;
+                            position: absolute;
+                            right: 0;
+                        }
+
+                    }
+                }
+                .location {
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                    height: 20px;
+                    img {
+                        height: 100%;
+                    }
+                }
+                .toolbar{
+                    width: 90%;
+                    height: 25px;
+                    text-align: center;
+                    .item {
+                        height: 100%;
+                        display: flex;
+                        width: 25%;
+                        justify-content: flex-start;
+                        align-items: center;
+                        float: left;
+                        img{
+                            height: 100%;
+                        }
+                        .count{
+                            margin-left: 5px;
+                        }
+                    }
+                }
+                .hot-comment {
+                    .content {
+                        float: left;
+                        width: 85%;
+                        .head-image {
+                            width: 40px;
+                            border-radius: 50%;
+                            float: left;
+                        }
+                        .comment-container {
+                            width: 85%;
+                            .time {
+                                color: gray;
+                            }
+                        }
+
+                    }
+                    .love {
+                        text-align: center;
+                        .love-image {
+                            width: 30px;
+                            border-radius: 50%;
+                        }
+                    }
+                }
+            }
+            .tag {
+                color: #E6C346;
+                margin: 0 3px;
+            }
+        }
+    }
+
+</style>
---
+++ src/pages/home/Message.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="message">
+        <div class="container p15p">
+            <div class="header">
+                <div class="title">
+                    <p class="tac">消息</p>
+                    <span>联系人</span>
+                </div>
+            </div>
+            <div class="toolbar mt20p">
+                <div>
+                    <img src="../../assets/img/icon/person.svg" alt="">
+                    <span>粉丝</span>
+                </div>
+                <div>
+                    <img src="../../assets/img/icon/love.svg" alt="">
+                    <span>赞</span>
+                </div>
+                <div>
+                    <img src="../../assets/img/icon/person.svg" alt="">
+                    <span>@我的</span>
+                </div>
+                <div>
+                    <img src="../../assets/img/icon/message.svg" alt="">
+                    <span>评论</span>
+                </div>
+            </div>
+            <div class="messages mt20p">
+                <div class="message oh mt15p">
+                    <div class="content pull-left ">
+                        <img src="../../assets/img/icon/head-image.jpeg" alt="" class="head-image pull-left">
+                        <div class="pull-right comment-container">
+                            <div class="name">游戏小助手</div>
+                            <div class="detail">抖出好游戏</div>
+                        </div>
+                    </div>
+                    <div class="love pull-right">
+                        <span class="f14 block">20:54</span>
+                    </div>
+                </div>
+                <div class="message oh mt15p">
+                    <div class="content pull-left ">
+                        <img src="../../assets/img/icon/head-image.jpeg" alt="" class="head-image pull-left">
+                        <div class="pull-right comment-container">
+                            <div class="name">抖音小助手</div>
+                            <div class="detail">抖音美好奇妙夜小贴士</div>
+                        </div>
+                    </div>
+                    <div class="love pull-right">
+                        <span class="f14 block">20:54</span>
+                    </div>
+                </div>
+                <div class="message oh mt15p">
+                    <div class="content pull-left ">
+                        <img src="../../assets/img/icon/head-image.jpeg" alt="" class="head-image pull-left">
+                        <div class="pull-right comment-container">
+                            <div class="name">TTentau</div>
+                            <div class="detail">其实我也不知道评论点什么啊啊啊看吧看吧看吧</div>
+                        </div>
+                    </div>
+                    <div class="love pull-right">
+                        <span class="f14 block">20:54</span>
+                    </div>
+                </div>
+            </div>
+            <p class="tac">无更多消息</p>
+        </div>
+        <Footer v-bind:init-tab="4" ref="footer" :style="{'top':height-footerHeight+'px'}"/>
+    </div>
+</template>
+
+<script>
+    import Footer from '../../components/Footer';
+
+    export default {
+        name: "Message",
+        components: {
+            Footer
+        },
+        data() {
+            return {
+                height:0,
+                width:0,
+                footerHeight:0
+            }
+        },
+        created() {
+            self = this;
+            this.height = document.body.clientHeight;
+            this.width = document.body.clientWidth;
+        },
+        mounted() {
+            let footer = this.$refs.footer.$el;
+            this.footerHeight = footer.offsetHeight;
+
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .message {
+        background: #2e3244;
+        color: #b8b9c1;
+        height: 100%;
+        .header {
+            .title {
+                position: relative;
+                span {
+                    position: absolute;
+                    right: 10px;
+                    top: 0;
+                }
+            }
+        }
+        .toolbar {
+            display: flex;
+            justify-content: space-around;
+            div {
+                text-align: center;
+                &:nth-child(1) {
+                    img {
+                        background: rgb(79, 136, 251);
+                    }
+                }
+                &:nth-child(2) {
+                    img {
+                        background: rgb(248, 53, 95);
+                    }
+                }
+                &:nth-child(3) {
+                    img {
+                        background: rgb(28, 194, 98);
+                    }
+                }
+                &:nth-child(4) {
+                    img {
+                        background: rgb(195, 106, 250);
+                    }
+                }
+                img {
+                    padding: 10px;
+                    border-radius: 3px;
+                    width: 25px;
+                    height: 25px;
+
+                }
+                span {
+                    display: block;
+                }
+            }
+
+        }
+        .messages {
+            border-top: 1px solid #494950;
+            .message {
+                .content {
+                    float: left;
+                    width: 85%;
+                    .head-image {
+                        width: 40px;
+                        border-radius: 50%;
+                        float: left;
+                    }
+                    .comment-container {
+                        width: 85%;
+                        .time {
+                            color: gray;
+                        }
+                    }
+
+                }
+                .love {
+                    text-align: center;
+                    .love-image {
+                        width: 30px;
+                        border-radius: 50%;
+                    }
+                }
+            }
+        }
+    }
+</style>
