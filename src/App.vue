3928d583ff3924a64add3bfd960867da94c030cb
zyronon <zyronon@163.com>
2021-07-04 03:55:48 +0800

    验证滑动视频闪屏问题

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@ export default {
 }
 </script>
 <style lang="scss">
+
+.base-slide-item {
+  min-width: 100vw;
+  min-height: 100vh;
+  position: relative;
+}
 #app {
   height: 100%;
   width: 100%;
