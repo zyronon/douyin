d592f952f13b3ed094cdc93ca31718b9cb79b967
ttentau <ttentau@163.com>
2021-07-04 03:55:48 +0800

    验证滑动视频闪屏问题

--- src/pages/home/Index2.vue
+++ src/pages/home/Index2.vue
@@ -1,43 +1,47 @@
 <template>
   <div id="home-index">
-    <SlideList direction="column"
-               ref="slideList"
-               @end="end"
-               @first="first"
-               :virtual="true"
-               :total="total"
-               @slide="slide"
+    <SlideListVirtual direction="column"
+                      ref="slideList"
+                      :virtual="true"
+                      :list="videos"
+                      :render="render"
     >
-      <SlideItem v-for="item in videos" :style="itemTop">
-        <video :src="item.videoUrl" ref="video" muted autoplay loop style="width: 100vw;height: 100vh;">
-          <p> 您的浏览器不支持 video 标签。</p>
-        </video>
-      </SlideItem>
-    </SlideList>
+    </SlideListVirtual>
   </div>
 </template>
 
 <script>
-import Comment from '../../components/Comment.vue'
-import Other from '../../components/Other.vue'
-import Share from '../../components/Share.vue'
-import Footer from "../../components/Footer.vue"
-import mp1 from '../../assets/video/1.mp4'
+import SlideListVirtual from './SlideListVirtual'
 import src1Bg from '../../assets/img/poster/src1-bg.png'
 import Video from "../../components/Video.vue";
-import mp41 from "../../assets/video/5.mp4";
+import mp40 from "../../assets/video/0.mp4";
+import mp41 from "../../assets/video/1.mp4";
 import mp42 from "../../assets/video/2.mp4";
 import mp43 from "../../assets/video/3.mp4";
 import mp44 from "../../assets/video/4.mp4";
 import mp45 from "../../assets/video/5.mp4";
+import mp46 from "../../assets/video/6.mp4";
+import mp47 from "../../assets/video/7.mp4";
+import mp48 from "../../assets/video/8.mp4";
+import mp49 from "../../assets/video/9.mp4";
+import mp410 from "../../assets/video/10.mp4";
 
 export default {
   name: "HomeIndex",
-  components: {Footer, Comment, Share, Other, Video},
+  components: {SlideListVirtual, Video},
   data() {
     return {
-      list: [1, 2, 3, 4, 5],
       videos: [
+        {
+          videoUrl: mp40,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: true,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
         {
           videoUrl: mp41,
           // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
@@ -88,94 +92,73 @@ export default {
           shared: 999,
           duration: 99
         },
+        {
+          videoUrl: mp46,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: false,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
+        {
+          videoUrl: mp47,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: false,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
+        {
+          videoUrl: mp48,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: false,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
+        {
+          videoUrl: mp49,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: false,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
+        {
+          videoUrl: mp410,
+          // videoUrl: 'http://babylife.qiniudn.com/FtRVyPQHHocjVYjeJSrcwDkApTLQ',
+          videoPoster: src1Bg,
+          isLoved: false,
+          loves: 1234,
+          comments: 666,
+          shared: 999,
+          duration: 99
+        },
       ],
-      addCount: 0,
-      total: 10,
-      isCommenting: false,
-      isSharing: false,
-      baseActiveIndex: 0,
-      activeIndex: 2,
-      tabWidth: 30,
-      tabIndexRelationActiveIndexLefts: [],
-      slideMoveXDistance: 0,
-      slideMoveYDistance: 0,
-      height: 0,
-      width: 0,
-      toolbarStyleTransitionDuration: 0,
-      loading: false
+      render: item => {
+        return (
+            `
+            <div class="base-slide-item">
+              <video src="${item.videoUrl}" ref="video" muted autoplay loop style="width: 100vw;height: 100vh;">
+              <p> 您的浏览器不支持 video 标签。</p>
+            </video>
+      </div>
+           `
+        )
+      }
     }
-  },
-  computed: {
-    itemTop() {
-      return {top: this.addCount * 812 + 'px'}
-    },
   },
   mounted() {
     this.height = document.body.clientHeight
     this.width = document.body.clientWidth
-  },
-  methods: {
-    end() {
-      if (this.slideMoveYDistance > 60) {
-        this.getData()
-      }
-      this.slideMoveYDistance = 0;
-      this.toolbarStyleTransitionDuration = 300
-    },
-    first(e) {
-      if (this.loading) return
-    },
-    getData() {
-      this.loading = true
-      setTimeout(() => {
-        this.loading = false
-      }, 100)
-    },
-    slide(e) {
-      let {currentSlideItemIndex, isDrawDown} = e
-      if (isDrawDown) {
-        if (this.videos.length - 3 < currentSlideItemIndex && currentSlideItemIndex + 2 < this.total) {
-          // this.list.push(currentSlideItemIndex + 3)
-          console.log('push')
-          this.videos.push({
-            videoUrl: require(`../../assets/video/${currentSlideItemIndex + 3}.mp4`),
-            videoPoster: src1Bg,
-            isLoved: true,
-            loves: 1234,
-            comments: 666,
-            shared: 999,
-            duration: 99
-          },)
-          // this.addCount += 1
-          // this.videos.shift()
-          setTimeout(() => {
-            // this.addCount += 1
-            // this.videos.shift()
-          }, 300)
-          this.$refs.slideList.checkChildren()
-        }
-      } else {
-        if (currentSlideItemIndex > 1 && currentSlideItemIndex + 3 < this.total) {
-          // this.videos.pop()
-          setTimeout(() => {
-            // this.addCount -= 1
-            this.videos.unshift({
-              videoUrl: require(`../../assets/video/${currentSlideItemIndex - 1}.mp4`),
-              videoPoster: src1Bg,
-              isLoved: true,
-              loves: 1234,
-              comments: 666,
-              shared: 999,
-              duration: 99
-            },)
-          }, 300)
-          this.$refs.slideList.checkChildren()
-        }
-      }
-    },
-  },
-  created() {
-
   },
 }
 </script>
