0adffcc8a18055227d0d1e3c9a1217ceb15cc580
zyronon <zyronon@163.com>
2021-07-09 17:50:28 +0800

    优化首页视频播放逻辑 我页面的一些子页面

---
+++ src/pages/me/EditUserInfo.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="edit">
+    <BaseHeader>
+      <template v-slot:center>
+        <div class="title">
+          <span class="f16">编辑资料</span>
+          <span class="sub f10">已完成85%</span>
+        </div>
+      </template>
+    </BaseHeader>
+    <div class="userinfo">
+      <div class="change-avatar">
+        <div class="avatar-ctn" @click="changeAvatarDialog = true">
+          <img class="avatar" src="../../assets/img/icon/head-image.jpeg" alt="">
+          <img class="change" src="../../assets/img/icon/back.png" alt="">
+        </div>
+        <span>点击更换头像</span>
+      </div>
+      <div class="row" @click="$nav('/editUserInfoItem',{type:1})">
+        <div class="left">名字</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row" @click="$nav('/editUserInfoItem',{type:2})">
+        <div class="left">抖音号</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row" @click="$nav('/editUserInfoItem',{type:3})">
+        <div class="left">简介</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row" @click="changeSexDialog = true">
+        <div class="left">性别</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="left">生日</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="left">所在地</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="left">学校</div>
+        <div class="right">
+          <span>B</span>
+          <img src="../../assets/img/icon/back.png" alt="">
+        </div>
+      </div>
+    </div>
+
+    <transition name="fade">
+      <div v-if="changeAvatarDialog" class="change-dialog" @click="changeAvatarDialog = false">
+        <div class="content">
+          <div class="item">拍一张</div>
+          <div class="item">从相册选择</div>
+          <div class="item">查看大图</div>
+          <div class="item">取消</div>
+        </div>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="changeSexDialog" class="change-dialog" @click="changeSexDialog = false">
+        <div class="content">
+          <div class="item">男</div>
+          <div class="item">女</div>
+          <div class="item">不展示</div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditUserInfo",
+  data() {
+    return {
+      changeAvatarDialog: false,
+      changeSexDialog: false,
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/scss/index";
+
+.title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .sub {
+    color: $second-text-color;
+  }
+}
+
+.userinfo {
+  color: white;
+
+  .change-avatar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 3rem 0;
+    $avatar-width: 7rem;
+
+    .avatar-ctn {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 1rem;
+
+      width: $avatar-width;
+      height: $avatar-width;
+
+      .avatar {
+        opacity: .5;
+        position: absolute;
+        width: $avatar-width;
+        height: $avatar-width;
+        border-radius: 50%;
+      }
+
+      .change {
+        z-index: 9;
+        position: relative;
+      }
+    }
+
+  }
+
+  .row {
+    padding: 0 1.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 5rem;
+    font-size: 1.4rem;
+
+    &:active {
+      opacity: .5;
+    }
+
+    .right {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: $second-text-color;
+
+
+      img {
+        margin-left: 1rem;
+        width: 15px;
+      }
+    }
+  }
+}
+
+.mask {
+}
+
+.change-dialog {
+  z-index: 10;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #000000bb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .content {
+    background: white;
+    width: 60%;
+    padding: .5rem 0;
+    box-sizing: border-box;
+
+    .item {
+      padding: 1.5rem 2rem;
+      transition: all .2s;
+
+      &:active {
+        background: darkgray;
+      }
+    }
+
+  }
+
+}
+</style>
---
+++ src/pages/me/EditUserInfoItem.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="edit-item">
+    <BaseHeader>
+      <template v-slot:center>
+        <span v-if="type === 1" class="f16">修改名字</span>
+        <span v-if="type === 2" class="f16">修改抖音号</span>
+        <span v-if="type === 3" class="f16">修改简介</span>
+      </template>
+      <template v-slot:right>
+        <div>
+          <span class="f16" :class="isChanged?'save-yes':'save-no'">保存</span>
+        </div>
+      </template>
+    </BaseHeader>
+
+    <div class="content">
+      <div v-if="type === 1">
+        <div class="notice">我的名字</div>
+        <div class="input-ctn" style="margin-bottom: 1rem;">
+          <input type="text" v-model="name" placeholder="记得填写名字哦">
+          <img class="close" src="../../assets/img/icon/close.svg" alt="">
+        </div>
+        <div class="num">{{ name.length }}/20</div>
+      </div>
+      <div class="row" v-if="type === 2">
+        <div class="notice">我的抖音号</div>
+        <div class="input-ctn" style="margin-bottom: 1rem;">
+          <input type="text" v-model="name">
+          <img class="close" src="../../assets/img/icon/close.svg" alt="">
+        </div>
+        <div class="num">最多16个字，只允许包含字母、数字、下划线和点，30天内仅能修改一次</div>
+      </div>
+      <div class="row" v-if="type === 3">
+        <div class="notice">个人简介</div>
+        <div class="textarea-ctn">
+        <textarea name="" id="" cols="30" rows="10"
+                  placeholder="填写个人简介更容易获得别人关注哦"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+//TODO 1、数据变了后，保存按钮变亮；2、数据变了，点返回，弹窗是否确认
+
+export default {
+  name: "EditUserInfo",
+  data() {
+    return {
+      type: 1,
+      name: 'B',
+      isChanged: false,
+    }
+  },
+  created() {
+    this.type = Number(this.$route.query.type)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/scss/index";
+
+.edit-item {
+
+  .save-yes {
+    color: $primary-btn-color;
+  }
+
+  .save-no {
+    color: $disable-primary-btn-color;
+  }
+
+  .content {
+    padding: 2rem;
+
+    .notice, .num {
+      font-size: 1.2rem;
+      color: $second-text-color;
+    }
+
+    .input-ctn {
+      position: relative;
+      border-bottom: 1px solid $line-color;
+      display: flex;
+      align-items: center;
+
+      input {
+        margin: .5rem 0;
+        color: white;
+        height: 3rem;
+        width: 100%;
+        outline: none;
+        border: none;
+        background: transparent;
+
+        &::placeholder {
+          color: $second-text-color;
+        }
+      }
+
+      .close {
+        position: absolute;
+        right: 0;
+        top: 1.5rem;
+        width: 1rem;
+      }
+    }
+
+    .textarea-ctn {
+      width: 100%;
+      background: rgb(100, 114, 158);
+      padding: 1.5rem;
+      box-sizing: border-box;
+      margin-top: 1rem;
+      border-radius: 2px;
+
+
+      textarea {
+        font-family: "Microsoft YaHei UI";
+        outline: none;
+        width: 100%;
+        border: none;
+        background: transparent;
+        color: white;
+
+        &::placeholder {
+          color: $second-text-color;
+        }
+      }
+    }
+  }
+
+
+}
+</style>
