d5754b1cdc245cbffd44a65317d9e51f598bc4a1
ttentau <ttentau@163.com>
2021-07-13 00:43:34 +0800

    选择院系

---
+++ src/pages/me/userinfo/ChooseDepartment.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="choose-school">
+    <BaseHeader>
+      <template v-slot:center>
+        <span class="f16">选择院系</span>
+      </template>
+      <template v-slot:right>
+        <span class="f14" @click="$nav('/declare-school',{type:2})">没有找到?</span>
+      </template>
+    </BaseHeader>
+    <div class="content">
+      <div class="nearby">
+        <div class="item"
+             v-for="item in departments"
+             @click="setDepartment(item)"
+        >{{ item }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import Search from '../../../components/Search'
+import {inject} from "vue";
+
+export default {
+  name: "ChooseSchool",
+  components: {
+    Search
+  },
+  data() {
+    return {
+      departments: [],
+      schoolName: '',
+    }
+  },
+  computed: {},
+  created() {
+    for (let i = 0; i < 5; i++) {
+      this.departments.push('院系' + i)
+    }
+  },
+  methods: {
+    setDepartment(val) {
+      localStorage.setItem('changeDepartment', val)
+      this.$back()
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../../assets/scss/index";
+
+.choose-school {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  color: white;
+  overflow: auto;
+
+
+  .content {
+    padding-top: 6rem;
+
+    .item {
+      padding: 0 2rem;
+      font-size: 1.4rem;
+      height: 5rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      transition: all .1s;
+      background: $main-bg;
+
+      &:active {
+        background: $active-main-bg;
+        color: $second-text-color;
+      }
+    }
+  }
+}
+
+</style>
--- src/pages/me/userinfo/DeclareSchool.vue
+++ src/pages/me/userinfo/DeclareSchool.vue
@@ -10,9 +10,16 @@
         <div class="label">学校全称</div>
         <input type="text" placeholder="请输入学校全称(必填)" v-model="form.name">
       </div>
-      <div class="row">
+      <div class="row" v-if="type === 1">
         <div class="label">所在城市</div>
         <input type="text" placeholder="请输入学校所在城市(必填)" v-model="form.location">
+      </div>
+      <div class="department-row" v-if="type === 2">
+        <div class="label">信息问题</div>
+        <div class="right">
+          <span>点击选择(必选)</span>
+          <img src="../../../assets/img/icon/back.png" alt="">
+        </div>
       </div>
       <div class="notice">感谢你的反馈，我们将尽快核对信息</div>
       <div class="btn" @click="submit">提交</div>
@@ -21,32 +28,31 @@
 </template>
 
 <script>
+//TODO　院系点击那个弹窗没做
 export default {
   name: "DeclareSchool",
   data() {
     return {
       form: {
         name: '',
-        location: ''
-      }
+        location: '',
+        departmentInfoType: ''
+      },
+      type: 1
     }
+  },
+  created() {
+    this.type = Number(this.$route.query.type)
   },
   methods: {
     submit() {
       if (!this.form.name) return this.$notice('请输入学校全称')
-      if (!this.form.location) return this.$notice('请输入学校所在城市')
+      if (this.type === 1 && !this.form.location) return this.$notice('请输入学校所在城市')
+      if (this.type === 2 && !this.form.departmentInfoType) return this.$notice('请选择信息问题')
       this.$notice('申报成功')
       setTimeout(this.$back, 1000)
     },
-    $notice(val) {
-      let div = document.createElement('div')
-      div.classList.add('global-notice')
-      div.textContent = val
-      document.body.append(div)
-      setTimeout(() => {
-        document.body.removeChild(div)
-      }, 1000)
-    }
+
   }
 }
 </script>
@@ -86,6 +92,32 @@ export default {
         border-bottom: 1px solid $line-color;
         color: white;
       }
+    }
+
+    .department-row {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .label {
+        color: white;
+        font-size: 1.6rem;
+        margin-right: 3rem;
+      }
+
+      .right {
+        padding: 1.2rem 0;
+        display: flex;
+        align-items: center;
+        color: $second-text-color;
+
+        img {
+          margin-left: 5px;
+          width: 1.2rem;
+        }
+      }
+
+
     }
 
     .notice {
