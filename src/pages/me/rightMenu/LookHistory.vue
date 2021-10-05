abc5b15ff58b2cb8b1c05df1a3c95af0dc0e73a5
zyronon <zyronon@163.com>
2021-10-05 17:59:39 +0800

    更新readme

--- src/pages/me/rightMenu/LookHistory.vue
+++ src/pages/me/rightMenu/LookHistory.vue
@@ -4,7 +4,7 @@
       <template v-slot:center>
         <span class="f16">观看历史</span>
       </template>
-      <template v-slot:right v-if="list">
+      <template v-slot:right v-if="isClear">
         <span class="second-text-color f13" @click="clear">清空</span>
       </template>
     </BaseHeader>
@@ -12,23 +12,28 @@
       <Indicator
           style="width: calc(100vw - 2rem); margin-left: 1rem;"
           tabStyleWidth="50%"
-          :tabTexts="['熟人列表','发现熟人']"
+          :tabTexts="['视频','影视综']"
           v-model:active-index="currentSlideItemIndex">
       </Indicator>
-      <SlideRowList v-model:active-index="currentSlideItemIndex" style="height: calc(100vh - 12rem);">
-        <SlideItem class="tab1"  style="overflow:auto;">
-          <Posters :list="list" v-if="list"></Posters>
-          <div class="empty" v-else>
-            <img src="../../../assets/img/icon/head-image.jpeg" alt="">
-            <div class="title">暂无内容</div>
-            <div class="subtitle">暂无观看历史记录</div>
-          </div>
+      <SlideRowList v-model:active-index="currentSlideItemIndex" class="SlideRowList">
+        <SlideItem class="tab1" style="overflow:auto;">
+          <Scroll class="Scroll"
+                  @pulldown="getHistoryVideo">
+            <Posters :list="historyVideo.list" v-if="historyVideo.total"></Posters>
+            <Loading :is-full-screen="false" v-if="loadingVideo"/>
+            <template v-else>
+              <NoMore v-if="historyVideo.list.length"/>
+              <div class="empty" v-else>
+                <img src="../../../assets/img/icon/none-bg1.webp" alt="">
+                <div class="title">暂无观看历史记录</div>
+              </div>
+            </template>
+          </Scroll>
         </SlideItem>
         <SlideItem class="tab2">
           <div class="empty">
-            <img src="../../../assets/img/icon/head-image.jpeg" alt="">
-            <div class="title">暂无内容</div>
-            <div class="subtitle">暂无观影历史记录</div>
+            <img src="../../../assets/img/icon/none-bg1.webp" alt="">
+            <div class="title">暂无观影历史记录</div>
           </div>
         </SlideItem>
       </SlideRowList>
@@ -37,26 +42,88 @@
 </template>
 <script>
 import Posters from "../../../components/Posters";
+import Scroll from "../../../components/Scroll";
+import NoMore from "../../../components/NoMore";
 
 export default {
   name: "lookHistory",
   components: {
-    Posters
+    NoMore,
+    Posters,
+    Scroll
   },
   data() {
     return {
+      loadingVideo: false,
+      loadingOther: false,
+      isClearHistoryVideo: false,
+      isClearHistoryOther: false,
       currentSlideItemIndex: 0,
-      list: 2,
+      pageSize: 15,
+      historyVideo: {
+        total: 0,
+        pageNo: 0,
+        list: []
+      },
+      historyOther: {
+        total: 0,
+        pageNo: 0,
+        list: []
+      }
     }
   },
-  computed: {},
+  computed: {
+    isClear() {
+      if (this.currentSlideItemIndex === 0) {
+        return this.historyVideo.list = []
+      }
+      return this.historyOther.list = []
+    }
+  },
   created() {
+    this.getHistoryVideo(true)
+    this.getHistoryOther(true)
   },
   methods: {
+    async getHistoryVideo(init = false) {
+      if (this.loadingVideo) return
+      if (this.isClearHistoryVideo) return
+      if (!init) {
+        if (this.historyVideo.total <= this.historyVideo.list.length) return
+        this.historyVideo.pageNo++
+      }
+      this.loadingVideo = true
+      let res = await this.$api.videos.historyVideo({pageNo: this.historyVideo.pageNo, pageSize: this.pageSize,})
+      console.log(res)
+      this.loadingVideo = false
+      if (res.code === this.SUCCESS) {
+        this.historyVideo.list = this.historyVideo.list.concat(res.data.list)
+        this.historyVideo.total = res.data.total
+      }
+    },
+    async getHistoryOther(init = false) {
+      if (this.loadingOther) return
+      if (this.isClearHistoryOther) return
+      this.loadingOther = true
+      if (!init) {
+        this.historyOther.pageNo++
+      }
+      let res = await this.$api.videos.historyOther({pageNo: this.historyOther.pageNo, pageSize: this.pageSize,})
+      this.loadingOther = false
+      if (res.code === this.SUCCESS) {
+        this.historyOther.list = this.historyOther.list.concat(res.data.list)
+        this.historyOther.total = res.data.total
+      }
+    },
     clear() {
-      this.$showConfirmDialog('确定清空？', '清空后，以往观看记录不再展示', 'gray',() => {
-        this.list = 0
-        console.log(this.list)
+      this.$showConfirmDialog('确定清空？', '清空后，以往观看记录不再展示', 'gray', () => {
+        if (this.currentSlideItemIndex === 0) {
+          this.historyVideo.list = []
+          this.isClearHistoryVideo = true
+          return
+        }
+        this.historyOther.list = []
+        this.isClearHistoryVideo = true
       })
     }
   }
@@ -79,19 +146,22 @@ export default {
   .content {
     padding-top: 6rem;
 
+    .SlideRowList, .Scroll {
+      height: calc(100vh - @indicator-height - @header-height) !important;
+    }
+
     .empty {
-      height: 100%;
+      height: 70vh;
       display: flex;
       align-items: center;
       justify-content: center;
       flex-direction: column;
 
-      .title {
-        margin-top: 1rem;
-        font-size: 1.5rem;
+      img {
+        height: 12rem;
       }
 
-      .subtitle {
+      .title {
         font-size: 1.3rem;
         margin-top: 1rem;
         color: @second-text-color;
