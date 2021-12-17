8f9842e0b2561f10b6d587feb1c4e428dbcd716a
ttentau <ttentau@163.com>
2021-12-17 17:44:28 +0800

    优化slide

--- src/pages/test/TestSlide.vue
+++ src/pages/test/TestSlide.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="TestSlide">
+    <SlideRowList
+        v-if="false"
+        name="baseSlide"
+        v-model:active-index="baseActiveIndex">
+      <SlideItem>
+        <SlideRowList
+            style="height: calc(100% - 5rem);"
+            v-model:active-index="activeIndex">
+          <SlideItem>
+            <div id="TestSlide1"></div>
+          </SlideItem>
+          <SlideItem>
+            <div id="TestSlide2"></div>
+          </SlideItem>
+        </SlideRowList>
+        <Footer v-bind:init-tab="1"/>
+      </SlideItem>
+      <SlideItem>
+        <div id="TestSlide3"></div>
+      </SlideItem>
+    </SlideRowList>
+    <div id="TestSlide1" style="height: calc(100% - 5rem);"></div>
   </div>
 </template>
 <script lang="jsx">
 import Slide from "./slide";
 import Video from "../../components/Video";
+import resource from "../../assets/data/resource.js";
 
 export default {
   name: "TestSlide",
@@ -15,8 +38,10 @@ export default {
   data() {
     return {
       totalSize: 52,
-      pageSize: 10,
+      pageSize: 30,
       pageNo: 0,
+      baseActiveIndex: 0,
+      activeIndex: 0,
     }
   },
   computed: {},
@@ -24,7 +49,11 @@ export default {
   created() {
   },
   mounted() {
-    let slide = new Slide('#TestSlide', {
+    let list = resource.videos
+    list = list.concat(resource.videos)
+    list.splice(24, 6)
+    console.log(list.length)
+    let slide = new Slide('#TestSlide1', {
       render: (item, itemIndex, play) => {
         return (
             <Video
@@ -39,24 +68,38 @@ export default {
             />
         )
       },
+      // list,
+      // index: 10,
       request: this.$api.videos.recommended
     })
+    // let slide2 = new Slide('#TestSlide2', {
+    //   render: (item, itemIndex, play) => {
+    //     return (
+    //         <Video
+    //             isPlay={play}
+    //             video={item}
+    //             index={itemIndex}
+    //             onShowComments={e => this.isCommenting = true}
+    //             onShowShare={e => this.isSharing = true}
+    //             onGoUserInfo={e => this.baseActiveIndex = 1}
+    //             onGoMusic={e => this.$nav('/home/music')}
+    //             v-model={[this.videos[itemIndex], 'video']}
+    //         />
+    //     )
+    //   },
+    //   request: this.$api.videos.recommended
+    // })
   },
   methods: {
     async getData() {
-      this.loading = true
       let res = await this.$api.videos.recommended({pageNo: this.pageNo, pageSize: this.pageSize})
       console.log(res)
-      this.loading = false
-      if (res.code === this.SUCCESS) {
-        this.totalSize = res.data.total
-        this.videos = this.videos.concat(res.data.list)
-        // this.videos = this.$clone(this.localVideos)
-      } else {
-        this.pageNo--
-      }
     },
   }
+}
+
+if (import.meta.hot) {
+
 }
 </script>
 <style>
