ad520fee34af6435f109d24263f5599bbeb73f6a
超哥 <ttentau@163.com>
2022-03-23 08:12:21 +0800

    优化slide

--- src/pages/slide/slide.vue
+++ src/pages/slide/slide.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="Slide">
+  <div class="Slide" @click.capture="c">
     <slide-horizontal>
       <div class="item">
         <slide-horizontal>
           <div class="item">
             <SlideVertical>
-              <div class="item">r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1r1</div>
+              <div class="item" v-for="i in videos">
+                <BVideo :video="i"></BVideo>
+              </div>
               <div class="item">r2222222222222222222222222222222222222222222222222222222</div>
               <div class="item">r333333333333333333333333333333333333333333333333333333</div>
             </SlideVertical>
@@ -36,21 +38,55 @@
 <script>
 import SlideHorizontal from './slide-horizontal'
 import SlideVertical from './slide-vertical'
+import BVideo from "../../components/BVideo";
+import resource from "../../assets/data/resource";
 
 export default {
   name: "slide",
   components: {
     SlideHorizontal,
-    SlideVertical
+    SlideVertical,
+    BVideo
   },
   data() {
-    return {}
+    return {
+      videos: [],
+      totalSize: 52,
+      pageSize: 10,
+      pageNo: 0,
+    }
   },
   created() {
+    this.getData()
   },
   mounted() {
   },
-  methods: {}
+  methods: {
+    c() {
+      console.log('console.log()')
+      return true
+    },
+    async getData() {
+      if (process.env.NODE_ENV !== 'development') {
+        this.totalSize = 11
+        // return this.videos = this.$clone(this.localVideos)
+        // await this.$sleep(200)
+        // return this.videos = this.$clone(this.webVideos)
+        return this.videos = this.$clone(resource.videos)
+      }
+      this.loading = true
+      let res = await this.$api.videos.recommended({pageNo: this.pageNo, pageSize: this.pageSize})
+      console.log(res)
+      this.loading = false
+      if (res.code === this.SUCCESS) {
+        this.totalSize = res.data.total
+        this.videos = this.videos.concat(res.data.list)
+        // this.videos = this.$clone(this.localVideos)
+      } else {
+        this.pageNo--
+      }
+    },
+  }
 }
 </script>
 
