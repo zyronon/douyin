954154d5fa29c6c5d458efe0b57578e9daa38f11
zyronon <zyronon@163.com>
2023-02-15 01:40:38 +0800

    商城

--- src/main.js
+++ src/main.js
@@ -9,20 +9,24 @@ import store from "./store";
 import mixin from "./utils/mixin";
 import VueLazyload from '@jambonn/vue-lazyload'
 import VConsole from 'vconsole';
+import {VueMasonryPlugin} from "vue-masonry";
 
 // const vConsole = new VConsole();
+const emitter = mitt()
 
 const app = Vue.createApp(App)
 app.config.globalProperties.$api = {...api}
+app.config.globalProperties.emitter = emitter
 app.config.unwrapInjectedRef = true
-const loadImage = new URL('./assets/img/icon/img-loading.png', import.meta.url).href
-app.provide('mitt', mitt())
+app.provide('mitt', emitter)
 app.mixin(mixin)
+const loadImage = new URL('./assets/img/icon/img-loading.png', import.meta.url).href
 app.use(VueLazyload, {
   preLoad: 1.3,
   loading: loadImage,
   attempt: 1
 })
+app.use(VueMasonryPlugin)
 app.use(router)
 app.use(store)
 app.mount('#app')
