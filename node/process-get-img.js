745429acaf7b03b54b31cefa90fd5b037af8412c
zyronon <zyronon@163.com>
2024-03-15 18:55:27 +0800

    优化

--- node/process-get-img.js
+++ node/process-get-img.js
@@ -9,19 +9,23 @@ let saveFileStr = fs.readFileSync(fileName, "utf8");
 let inputData = JSON.parse(saveFileStr);
 
 
-const downloadImage = (src, dest, callback) => {
-  request.head(src, (err, res, body) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    src &&
-    request(src)
-      .pipe(fs.createWriteStream(dest))
-      .on("close", () => {
-        callback && callback(null, dest);
-      });
-  });
+const downloadImage = async (src, dest, callback) => {
+  console.log('下载:', src, dest, Date.now())
+  return new Promise(resolve => {
+    request.head(src, (err, res, body) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      src &&
+      request(src)
+        .pipe(fs.createWriteStream(dest))
+        .on("close", () => {
+          setTimeout(resolve, 1000)
+          callback && callback(null, dest);
+        });
+    });
+  })
 };
 
 let saveFilePath = './imgs/'
@@ -32,14 +36,45 @@ async function sleep(val) {
   })
 }
 
-inputData.slice(0, 2).map(async v => {
-  await v.cover_url.map(async a => {
+async function test(list) {
+  for (let j = 0; j < list.length; j++) {
+    let a = list[j]
     let coverUrl = a.url_list[a.url_list.length - 1]
     a.url_list = [coverUrl]
-    console.log('coverUrl', coverUrl)
-    await sleep(1500)
-    downloadImage(coverUrl, saveFilePath + nanoid() + '.png')
-  })
-})
+    if (coverUrl.includes('http')) {
+      let name = nanoid() + '.png'
+      await downloadImage(coverUrl, saveFilePath + name, () => {
+        // console.log('close', name)
+        a.url_list = [name]
+        fs.writeFileSync(fileName, JSON.stringify(inputData, null, 2));
+      })
+    }
+  }
+}
 
-fs.writeFileSync(fileName, JSON.stringify(inputData, null, 2));
+async function test2(list) {
+  for (let j = 0; j < list.length; j++) {
+    let coverUrl = list[list.length - 1]
+    if (coverUrl.includes('http')) {
+      let name = nanoid() + '.png'
+      await downloadImage(coverUrl, saveFilePath + name, () => {
+        // console.log('close', name)
+        fs.writeFileSync(fileName, JSON.stringify(inputData, null, 2));
+      })
+    }
+    return [coverUrl]
+  }
+}
+
+for (let i = 0; i < inputData.slice(0, 111).length; i++) {
+  let v = inputData[i]
+  await test(v.cover_url)
+  await test(v.white_cover_url)
+  fs.writeFileSync(fileName, JSON.stringify(inputData, null, 2));
+  if (v.share_info && v.share_info.share_image_url && v.share_info.share_image_url.url_list) {
+    let r = await test2(v.share_info.share_image_url.url_list)
+    v.share_info.share_image_url.url_list = r
+    fs.writeFileSync(fileName, JSON.stringify(inputData, null, 2));
+  }
+}
+
