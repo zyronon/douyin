9917769b65628423d6d15f2babfc3edfadb750ce
zyronon <zyronon@163.com>
2024-03-27 15:50:33 +0800

    save

--- vite.config.js
+++ vite.config.js
@@ -133,10 +133,26 @@ export default defineConfig({
           if (id.includes('/src/pages/shop/GoodsDetail.vue')) return 'other'
 
           if (id.includes('/src/pages/message/Message.vue')) return 'other'
+          if (id.includes('/src/pages/message/Fans.vue')) return 'other'
+          if (id.includes('/src/pages/message/AllMessage.vue')) return 'other'
+          if (id.includes('/src/pages/message/notice/DouyinHelper.vue')) return 'other'
+          if (id.includes('/src/pages/message/notice/SystemNotice.vue')) return 'other'
+          if (id.includes('/src/pages/message/notice/TaskNotice.vue')) return 'other'
+          if (id.includes('/src/pages/message/notice/LiveNotice.vue')) return 'other'
+          if (id.includes('/src/pages/message/notice/MoneyNotice.vue')) return 'other'
 
           if (id.includes('/src/pages/me/Me.vue')) return 'other'
+          if (id.includes('/src/pages/me/Visitors.vue')) return 'other'
+          if (id.includes('/src/pages/me/RequestUpdate.vue')) return 'other'
+          if (id.includes('/src/pages/me/userinfo/EditUserInfo.vue')) return 'other'
+          if (id.includes('/src/pages/me/userinfo/EditUserInfoItem.vue')) return 'other'
+          if (id.includes('/src/pages/me/MyMusic.vue')) return 'other'
+
           if (id.includes('/src/pages/other/VideoDetail.vue')) return 'other'
           if (id.includes('/src/pages/other/AlbumDetail.vue')) return 'other'
+
+          if (id.includes('/src/pages/people/FindAcquaintance.vue')) return 'other'
+          if (id.includes('/src/pages/people/FollowAndFans.vue')) return 'other'
         },
         chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
         entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
