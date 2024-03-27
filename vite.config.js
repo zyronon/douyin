6a629dd65bb871ed9ac3b94d12912415904ae45d
zyronon <zyronon@163.com>
2024-03-27 23:42:52 +0800

    save

--- vite.config.js
+++ vite.config.js
@@ -61,46 +61,45 @@ export default defineConfig({
         }
       ],
     }),
-    // viteCompression({
-    //   verbose: false,
-    //   disable: false,
-    //   threshold: 10240,
-    //   algorithm: 'brotliCompress',
-    // }),
-    // viteCompression({
-    //   verbose: false,
-    //   disable: false,
-    //   algorithm: 'gzip',
-    //   threshold: 10240,
-    // })
-    //图片压缩
-    // viteImagemin({
-    //   gifsicle: {
-    //     optimizationLevel: 7,
-    //     interlaced: false,
-    //   },
-    //   optipng: {
-    //     optimizationLevel: 7,
-    //   },
-    //   mozjpeg: {
-    //     quality: 20,
-    //   },
-    //   pngquant: {
-    //     quality: [0.8, 0.9],
-    //     speed: 4,
-    //   },
-    //   svgo: {
-    //     plugins: [
-    //       {
-    //         name: 'removeViewBox',
-    //       },
-    //       {
-    //         name: 'removeEmptyAttrs',
-    //         active: false,
-    //       },
-    //     ],
-    //   },
-    // }),
+    viteCompression({
+      verbose: false,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'brotliCompress',
+    }),
+    viteCompression({
+      verbose: false,
+      disable: false,
+      algorithm: 'gzip',
+      threshold: 10240,
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
