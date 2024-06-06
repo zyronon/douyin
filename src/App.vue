0478cc868cb8a743e9f18e255ea25481551e38fa
Zyronon <zyronon@163.com>
2024-06-06 16:46:04 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ watch(
 function resetVhAndPx() {
   let vh = window.innerHeight * 0.01
   document.documentElement.style.setProperty('--vh', `${vh}px`)
-  document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 + 'px'
+  //document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 + 'px'
 }
 
 onMounted(() => {
